99c06fdd4d1dd601f4cc78be88950de1d3765ef5
Alexandre Tadashi Sato <alexandresato@outlook.com>
2023-01-31 09:46:43 -0300

    Ajustes

--- lib/features/image_dog/presentation/bloc/image_dog_event.dart
+++ lib/features/image_dog/presentation/bloc/image_dog_event.dart
@@ -2,12 +2,8 @@ part of 'image_dog_bloc.dart';
 
 @immutable
 abstract class ImageDogEvent extends Equatable {
-
-@override
-  List<Object> get props => [];  
+  @override
+  List<Object> get props => [];
 }
 
-class GetImageDogEvent extends ImageDogEvent {
- 
- 
-}
+class GetImageDogEvent extends ImageDogEvent {}
